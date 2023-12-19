9e9d2263bb6a52068c7a37d5420423a4c38543ec
ahaostudy <1993584108@qq.com>
2023-12-19 16:50:04 +0800

    style: add license header

--- gift-analyze/src/main/java/org/giftorg/analyze/dao/FunctionESDao.java
+++ gift-analyze/src/main/java/org/giftorg/analyze/dao/FunctionESDao.java
@@ -1,15 +1,15 @@
 /**
  * Copyright 2023 GiftOrg Authors
- * <p>
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,9 +19,6 @@
 
 package org.giftorg.analyze.dao;
 
-import cn.hutool.core.lang.func.Func;
-import co.elastic.clients.elasticsearch.core.SearchResponse;
-import co.elastic.clients.elasticsearch.core.search.Hit;
 import lombok.extern.slf4j.Slf4j;
 import org.giftorg.analyze.entity.Function;
 import org.giftorg.common.elasticsearch.Elasticsearch;
